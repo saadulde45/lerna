8c08caff693c2872c4a9401870416436d4e9774f
Saad Ulde <saad.ulde@globant.com>
2019-06-18 23:16:59 +0530

    1. Changing structure of app from functional to stateful component 2. Adding react-scripts to components 3. Adding build scripts to components

---
+++ packages/npm-app1/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NpmApp1 from './npm-app1';
+
+ReactDOM.render(<NpmApp1 />, document.getElementById('root'));
---
+++ packages/npm-app1/src/npm-app1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class NpmApp1 extends React.Component {
+    render() {
+        console.log('integrated in parent final');
+        return (
+            <div>
+                Child
+        </div>
+        );
+    }
+}
+
+export default NpmApp1;
