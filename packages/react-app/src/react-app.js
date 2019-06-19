c459c877a40853e0e6a2f04b782a1e8f3619712e
Saad Ulde <saad.ulde@globant.com>
2019-06-19 18:18:28 +0530

    1. Removing console log

--- packages/react-app/src/react-app.js
+++ packages/react-app/src/react-app.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 class ReactApp extends React.Component {
     render() {
-        console.log('integrated in parent final');
         return (
             <div>
                 React Child
