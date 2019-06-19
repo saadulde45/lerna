74058316fb87fad70d6e68af2ce8430ac9d27f05
Saad Ulde <saad.ulde@globant.com>
2019-06-19 18:10:27 +0530

    1. Updating references to the renamed component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactApp from "react-app";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Test parent
+            <ReactApp></ReactApp>
+            </div>
+        );
+    }
+};
+
+export default App;
