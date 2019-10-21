c83f2845173f9c7782afbacf324ed0bc2a0bbd29
Saad Ulde <saad.ulde@globant.com>
2019-10-21 19:13:44 +0530

    1. Bumping wc-angular-app version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from "react";
 import WCReactApp from "wc-react-app";
 
+function bootstrapApp() {
+  import(/* webpackChunkName: "angularApp" */ 'wc-angular-app/dist/wc-angular-app/main.js');
+}
+
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                Test parent
-            <WCReactApp></WCReactApp>
-            </div>
-        );
-    }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'start'
+    };
+  }
+
+  render() {
+    return (
+      <div id="test">
+        Test parent
+        <WCReactApp></WCReactApp>
+        <angular-app></angular-app>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    bootstrapApp();
+  }
+
 };
 
 export default App;
